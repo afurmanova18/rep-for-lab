5621511ca3257b5c4a0fde4d5e09da01ebe1276b
afurmanova18 <91338592+afurmanova18@users.noreply.github.com>
2021-10-30 11:21:44 +0300

    add LaterInYear in date.cpp

--- date.cpp
+++ date.cpp
@@ -5,3 +5,11 @@ bool IsLeapYear(int year)
 		return true;
 	return false;
 }
+
+bool LaterInYear(int d1, int mo1, int d2, int m2)
+{
+	if (m1 > m2) 
+           return true;
+	else 
+           return (d1 > d2);
+}
