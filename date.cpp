505b4ce61f5045a3e47a50d6a3ae4dcb99b6ccf0
afurmanova18 <91338592+afurmanova18@users.noreply.github.com>
2021-10-30 11:09:12 +0300

    add IsLeapYear and SecondsInMinute in date.cpp

--- date.cpp
+++ date.cpp
@@ -1,1 +1,12 @@
 #include "date.h"
+bool IsLeapYear(int year)
+{
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 400 == 0))
+		return true;
+	return false;
+}
+
+int SecondsInMinute(int sec)
+{
+	return sec / 60;
+}
